19c8b78a3943ff552f0d3efd66455095ffb6ac17
Madhu <Madhusudhan.V@icdl.org>
2025-02-17 20:32:36 +0530

    Client to Callback parameters passing improved using querystring works for all the best ways

--- LinkedinSharingPostsAspBlazor/Controllers/LinkedinController.cs
+++ LinkedinSharingPostsAspBlazor/Controllers/LinkedinController.cs
@@ -1,37 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
 
-namespace LinkedinSharingPostsAspBlazor.Controllers
+namespace LinkedinSharingPostsAspBlazor.Controllers;
+
+//[ApiController]
+[Route("[controller]")]
+public class LinkedinController(LinkedInService linkedInService) : ControllerBase
 {
-    //[ApiController]
-    [Route("[controller]")]
-    public class LinkedinController(LinkedInService linkedInService, IMemoryCache cache) : ControllerBase
+    [HttpGet(nameof(CallBack))]
+    public async Task CallBack()
     {
-        [HttpGet(nameof(CallBack))]
-        public async Task CallBack()
+        var context = this.HttpContext;
+        var code = context.Request.Query["code"];
+        if (string.IsNullOrEmpty(code)) throw new Exception("Code Is Not Passed");
+
+        var accessToken = await linkedInService.GetAccessTokenAsync(code!);
+        var personId = await linkedInService.GetPersonIdAsync(accessToken);
+
+       //cache can be used but in case of detached mode of clinet server it makes problem
+        var queryParameters = LinkedInPost.ParseState(context.Request.Query[nameof(LinkedInPost.state)]);
+        if (queryParameters != null)
         {
-            var context = this.HttpContext;
-            var code = context.Request.Query["code"];
+            queryParameters.TryGetValue(nameof(LinkedInPost.Text), out string text);
+            queryParameters.TryGetValue(nameof(LinkedInPost.ImageUrlRemote), out string imageUrl);
+            queryParameters.TryGetValue(nameof(LinkedInPost.TextUrl), out string url);
+            queryParameters.TryGetValue(nameof(LinkedInPost.ReturnUrl),out string returnUrl);
 
-            var accessToken = await linkedInService.GetAccessTokenAsync(code);
-            var personId = await linkedInService.GetPersonIdAsync(accessToken);
-
-            // Retrieve the text and imageUrl from the memory cache
-            //var cache = context.RequestServices.GetRequiredService<IMemoryCache>();
-            var text = cache.Get<string>(LinkedInPost.TextConst);
-            var imageUrl = cache.Get<string>(LinkedInPost.ImageUrlRemoteConst);
-            var url = cache.Get<string>(LinkedInPost.UrlConst);
-
-            string? assetUrl = null;
-            if (imageUrl != null)//imageBytes != null || 
+            string? uploadedAssetUrl = null;
+            if (!string.IsNullOrEmpty(imageUrl))//imageBytes != null || 
             {
-                (string uploadUrl, assetUrl) = await linkedInService.RegisterUploadAsync(accessToken, personId);
+                (string uploadUrl, uploadedAssetUrl) = await linkedInService.RegisterUploadAsync(accessToken, personId);
 
                 await linkedInService.UploadImageAsync(uploadUrl, imageBytes: null, remoteImageUrl: imageUrl);
             }
 
-            await linkedInService.SharePostAsync(accessToken, personId, text, assetUrl, url);
-            context.Response.Redirect("/");
+            await linkedInService.SharePostAsync(accessToken, personId, text, uploadedAssetUrl, url);
+            context.Response.Redirect(returnUrl??"/");
         }
+        context.Response.Redirect("/");
     }
 }
--- LinkedinSharingPostsAspBlazor/LinkedinService.cs
+++ LinkedinSharingPostsAspBlazor/LinkedinService.cs
@@ -179,16 +179,61 @@ public class LinkedInService(IHttpClientFactory httpClientFactory, IConfiguratio
 
 public class LinkedInPost
 {
-    public const string TextConst = "LinkedInPostText";
-    public const string UrlConst = "LinkedInPostUrl";
-    public const string ImageUrlRemoteConst = "LinkedInPostImageUrlRemote";
-    public const string ImageUrlLocalConst = "LinkedInPostImageUrlLocal";
-
-    public string Text { get; set; } = "Default Testing Text Here only";
+    public LinkedInPost() { }
+    public LinkedInPost(string clientId, string redirectUri)
+    {
+        this.ClientId = clientId;
+        this.RedirectUri = redirectUri;
+    }
+    public string ClientId { get; set; } = default!;
+    public string RedirectUri { get; set; } = default!;
+    public string Text { get; set; } = "Default Linkedin Post Testing Text Here only";
     public string ImageUrlRemote { get; set; } = "https://www.w3schools.com/howto/img_nature.jpg";
-    public string? ImageUrlLocal { get; set; }//for uploading from local server
+    //public string? ImageUrlLocal { get; set; }//for uploading from local server
     public string TextUrl { get; set; } = "SmartTown.in";
+    public string ReturnUrl { get; set; } = "/";//Encoded("/");
 
+    public string GetAuthorizationUrl()// "%2F" means "/"
+    {
+        string queryParameters = string.Empty;
+        if (!string.IsNullOrEmpty(Text) || !string.IsNullOrEmpty(ImageUrlRemote) || !string.IsNullOrEmpty(TextUrl))
+        {
+            //"Text=DefaultTest%20Of%20Linkedin%20Post%20Testing%20Text%20Here%20only&ImageUrlRemote=https%3A%2F%2Fwww.w3schools.com%2Fhowto%2Fimg_nature.jpg&TextUrl=SmartTown.in&ReturnUrl=%2FCounter"
+
+            if (!string.IsNullOrEmpty(Text))
+                queryParameters += $"{nameof(Text)}={Uri.EscapeDataString(Text)}";
+            if (!string.IsNullOrEmpty(ImageUrlRemote))
+                queryParameters += $"&{nameof(ImageUrlRemote)}={Uri.EscapeDataString(ImageUrlRemote)}";
+            if (!string.IsNullOrEmpty(TextUrl))
+                queryParameters += $"&{nameof(TextUrl)}={Uri.EscapeDataString(TextUrl)}";
+            if (!string.IsNullOrEmpty(ReturnUrl))
+                queryParameters += $"&{nameof(ReturnUrl)}={Uri.EscapeDataString(ReturnUrl)}";
+        }
+
+        //"https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78y4e4touu7uv8&redirect_uri=https://localhost:7244/linkedin/callback&scope=openid%20profile%20email%20r_basicprofile%20w_member_social&state=Text%3DDefaultTest%2520Of%2520Linkedin%2520Post%2520Testing%2520Text%2520Here%2520only%26ImageUrlRemote%3Dhttps%253A%252F%252Fwww.w3schools.com%252Fhowto%252Fimg_nature.jpg%26TextUrl%3DSmartTown.in%26ReturnUrl%3D%252FCounter"
+
+        var url = $"{AuthorizeCodeUrl}{ClientId}&redirect_uri={RedirectUri}&scope={Scopes}&{nameof(state)}={Uri.EscapeDataString(queryParameters)}";
+        return url;
+    }
+
+    public static Dictionary<string, string>? ParseState(string state)
+    {
+        if (!string.IsNullOrEmpty(state))
+            return Uri.UnescapeDataString(state).Split('&')
+                               .Select(part => part.Split('='))
+                               .ToDictionary(split => split[0], split => split[1]);
+        else return null;
+    }
+
+    //var authorizationUrl = $"https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={Configuration["LinkedIn:ClientId"]}&redirect_uri={Configuration["LinkedIn:RedirectUri"]}&scope=openid%20profile%20email%20r_basicprofile%20w_member_social";
+
+    public const string AuthorizeCodeUrl = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=";
+    //Uri.EscapeDataString("openid profile email r_basicprofile w_member_social");
+    public const string Scopes = "openid%20profile%20email%20r_basicprofile%20w_member_social";
+    public const string state = "state";
+
+    public static string GetAuthorizationUrl(string clientId, string redirectUri, string returnUrl = "%2F")// "%2F" means "/"
+    => $"{AuthorizeCodeUrl}{clientId}&redirect_uri={Uri.EscapeDataString($"?returnUrl={returnUrl}")}&scope={Scopes}";
 }
 
 
